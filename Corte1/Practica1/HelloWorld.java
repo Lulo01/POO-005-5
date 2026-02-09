40f6ee6859b169ed1b8fe93425807f84723b91a3
Lulo <luciana.gomez0110234@gmail.com>
2026-02-09 13:40:24 -0500

    Add files via upload

---
+++ Corte1/Practica1/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+/**
+ *
+ * @author Luloo
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        // TODO code application logic here
+        
+    }
+    
+}
